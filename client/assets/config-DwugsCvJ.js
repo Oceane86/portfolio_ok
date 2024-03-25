5d88c7c2c13c8e340dd1deae72a02a794753bbb0
Oceane86 <124265385+Oceane86@users.noreply.github.com>
2024-03-25 19:53:52 +0100

    Delete client directory

--- client/assets/config-DwugsCvJ.js
+++
@@ -1,5 +0,0 @@
-const o="Océane Rakotomalala",n="Développeur",t=["Intégration","Graphisme"],i="https://oceane86.github.io/portfolio_ok/index.html",e="Oceane86",c="https://github.com/Oceane86/portfolio_ok",s=`__  __  __
-\\ \\ \\ \\ \\∕
- \\ \\∕\\ \\
-  \\∕  \\∕
-`,a={name:o,role:n,disciplines:t,url:i,github:e,repo:c,ascii:s};export{a as c};
--- client/assets/yaspeez-screen-large-DTY4msaF.js
+++
@@ -1,1 +0,0 @@
-const e="/assets/yaspeez-screen-large-DpVx_5Og.webp";export{e as s};
